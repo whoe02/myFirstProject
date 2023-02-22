8c2f930abec3ad8fb381e3379fec20e2f921e46c
anreh <anurehka@gmail.com>
2023-02-22 11:57:04 +0800

    Commit on 22/2/2023 Push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Get the Button view from the layout and assign a click
+        //listener to it
+
+        val rollButton: Button = findViewById( R.id.rollButton)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice(){
+        Toast.makeText(this,"Dice is rolled",Toast.LENGTH_SHORT).show()
+        val randomInt=(1..6).random()
+        val resultText: TextView = findViewById(R.id.txtNumber)
+
+        resultText.text = randomInt.toString()
+
     }
 }
